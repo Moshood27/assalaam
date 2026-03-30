0cc2204eaf428fbae6459302bab93de7fd932f55
moshood <madewale5@gmail.com>
2026-03-30 15:57:39 +0100

    DEPLOY: Clean build with all assets

--- backend/app/Filament/Resources/ProjectProfitResource/Pages/ListProjectProfits.php
+++ backend/app/Filament/Resources/ProjectProfitResource/Pages/ListProjectProfits.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\ProjectProfitResource\Pages;
 
 use App\Filament\Resources\ProjectProfitResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProjectProfits extends ListRecords
 {
     protected static string $resource = ProjectProfitResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
