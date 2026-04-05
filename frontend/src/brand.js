9f4d19388b2aa2efd83c1d1bb885e236caf43109
moshood <madewale5@gmail.com>
2026-04-05 16:14:12 +0100

    DEPLOY: Clean build with all assets

--- frontend/src/brand.js
+++ frontend/src/brand.js
@@ -12,7 +12,7 @@ const darkLogo = `${prefix}images/${slug}-logo-dark.svg`
 const favicon = `${prefix}images/${slug}-favicon.svg`
 
 // Theme color for OS chrome (status bar, PWA address bar)
-const themeColor = slug === 'attaqwa' ? '#3730a3' /* indigo-700 */ : '#065f46' /* emerald-800 */
+const themeColor = slug === 'attaqwa' ? '#3730a3' /* indigo-700 */ : '#0f766e' /* teal-700 */
 
 const shortName = slug === 'attaqwa' ? 'Attaqwa' : 'Assalam'
 
