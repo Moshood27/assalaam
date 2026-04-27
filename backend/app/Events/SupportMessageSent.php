8f92bcbcdc0d4d54443b837f0996248da4f9d9e1
moshood <madewale5@gmail.com>
2026-04-27 15:59:19 +0100

    DEPLOY: Clean build with all assets

--- backend/app/Events/SupportMessageSent.php
+++ backend/app/Events/SupportMessageSent.php
@@ -6,10 +6,10 @@ use App\Models\SupportMessage;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Queue\SerializesModels;
 
-class SupportMessageSent implements ShouldBroadcastNow
+class SupportMessageSent implements ShouldBroadcast
 {
     use InteractsWithSockets, SerializesModels;
 
