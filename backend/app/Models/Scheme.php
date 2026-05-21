9fe0550a2955a5b826a8a1b63781a338b5c6e666
moshood <madewale5@gmail.com>
2026-05-21 13:13:36 +0100

    DEPLOY: Clean build with all assets

--- backend/app/Models/Scheme.php
+++ backend/app/Models/Scheme.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-use Spatie\Activitylog\Traits\LogsActivity;
-use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Support\LogOptions;
 
 class Scheme extends Model
 {
